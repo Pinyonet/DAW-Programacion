cdacb6a2b23412d1570833a3938708ce214b9c67
Pinyonet <46427657+Pinyonet@users.noreply.github.com>
2022-11-23 19:08:02 +0100

    Add files via upload

---
+++ Prog03_Ejerc2/src/main/java/com/prog03/Principal/Prog03_Ejerc2.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.prog03.Principal;
+
+import com.prog03.figuras.Rectangulo;
+import java.util.Scanner;
+
+/**
+ *
+ * @author hecto
+ */
+public class Prog03_Ejerc2 {
+
+    public static void main(String[] args) {
+        
+         Scanner teclado=new Scanner(System.in); //Creamos teclado para recoger datos por teclado
+        
+        
+        
+        Rectangulo rect = new Rectangulo(0, 0);  //Creamos objeto Rect
+        
+         System.out.println("Introduce un base:");   //Recogemos datos base
+        rect.setBase(teclado.nextInt());
+        
+         System.out.println("Introduce un altura:");  //Recogemos datos altura
+        rect.setAltura(teclado.nextInt());
+        
+        
+        System.out.println("Rectángulo creado: \n" + rect); //Utilizamos método toString.
+        System.out.println("Area: " + rect.getArea()); //Utilizamos método getArea
+        System.out.println(rect.isCuadrado()? "Es cuadrado":"No es cuadrado"); //Utilizamos metodo cuadrado
+    }
+}
---
+++ Prog03_Ejerc2/src/main/java/com/prog03/figuras/Rectangulo.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.prog03.figuras;
+
+/**
+ *
+ * @author hecto
+ */
+public class Rectangulo { 
+    
+    //declaramos atributos
+    private int base; 
+    private int altura;
+
+    public Rectangulo(int base, int altura) {  //Creamos constructor
+    this.base = base;
+    this.altura = altura;
+    }
+
+    //Creamos Gets
+    
+    public double getBase() { 
+    return base;
+    }
+
+    public double getAltura() {
+    return altura;
+    }
+
+    //Creamos Sets
+    
+    public void setBase(int base) {
+    this.base = base;
+    }
+
+    public void setAltura(int altura) {
+    this.altura = altura;
+    }
+    
+    
+    //Creamos metodo getArea
+    public float getArea(){
+    float area=(base*altura);
+    return area;
+    }
+
+    
+    
+    //Creamos metodo toString()
+    @Override
+    public String toString() {
+    return "La base del rectangulo = " + getBase() + "\nLa altura del rectangulo = " + getAltura() ;
+    }
+
+    //Creamos booleano solicitado
+    
+    public boolean isCuadrado(){
+    return(base == altura);
+    }
+}
